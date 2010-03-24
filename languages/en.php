d5e7533b94c772b4d3e1ae22d7b52df9370ad553
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-24 11:21:17 +0000

    naming changed to channels
    git-svn-id: http://code.elgg.org/plugins@5492 36083f99-b078-4883-b0ff-0f9b5a30f544

--- languages/en.php
+++ languages/en.php
@@ -11,40 +11,40 @@
 
 $english = array(
 	/** Generic **/
-	'shared_access:shared_access' => 'Shared Access',
-	'shared_access:shared' => 'Shared',
+	'shared_access:shared_access' => 'Channels',
+	'shared_access:shared' => 'Channel',
 	'shared_access:members' => 'Members',
 	'shared_access:shared_access_name' => 'Name',
 	'shared_access:shared_access_description' => 'Description',
 	'shared_access:no_shared_content' => 'No shared content found.',
-	'shared_access:back_home' => 'Back to All Collections',
+	'shared_access:back_home' => 'Back to all channels',
 	'shared_access:notifications' => 'Notification',
-	'shared_access:post_to_wire' => 'Post to Collection Wire',
+	'shared_access:post_to_wire' => 'Post to channel wire',
 	'shared_access:post_to' => 'Post to',
-	'shared_access:no_shared_access_collections' => 'No shared access collections.',
-	'shared_access:collections' => 'Collections',
-	'shared_access:info' => 'Shared access is a lightweight way to collaborate around an access level.',
+	'shared_access:no_shared_access_collections' => 'No channels',
+	'shared_access:collections' => 'Channels',
+	'shared_access:info' => 'Channels are a lightweight way to filter content based on an access level.',
 
 	/** Admin **/
-	'shared_access:new_collection' => 'Add a new collection',
+	'shared_access:new_collection' => 'Add a new channel',
 	'shared_access:member_count_singular' => '%s member',
 	'shared_access:member_count' => '%s members',
 	'shared_access:collection_members' => 'Members',
 	'shared_access:invite_users' => 'Invite Users',
-	'shared_access:invited_to_sac' => 'You have been invited to join this collection: ',
+	'shared_access:invited_to_sac' => 'You have been invited to join this channel: ',
 	'shared_access:declined' => 'You have declined the invitation.',
 	'shared_access:joined' => 'You have joined the invitation.',
-	'shared_access:collection_saved' => 'Collection saved.',
+	'shared_access:collection_saved' => 'Channel saved.',
 	'shared_access:decline' => 'Decline',
 	'shared_access:join' => 'Join',
-	'shared_access:details' => 'Details',
-	'shared_access:leave' => 'Leave',
+
+	'shared_access:show_in_tools_menu' => 'Show link in tools menu?',
 
 	/** Emails **/
 	'shared_access:request_join_subject' => '%s wants you to join %s!',
 	'shared_access:request_join_body' => 'Hi %s,
 
-You have been invited to share content with the "%s" Shared Access Group.  Click below to accept the invitation:
+You have been invited to share content with the "%s" channel.  Click below to accept the invitation:
 
 %s",',
 
